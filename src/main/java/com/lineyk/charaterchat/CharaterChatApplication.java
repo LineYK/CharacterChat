c050443078180841da6fcde3337b47f7524a2365
LineYK <yhoung11@gmail.com>
2026-03-05 00:49:34 +0900

    init.

---
+++ src/main/java/com/lineyk/charaterchat/CharaterChatApplication.java
@@ -0,0 +1,13 @@
+package com.lineyk.charaterchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharaterChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharaterChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lineyk/charaterchat/CharaterChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineyk.charaterchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharaterChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
