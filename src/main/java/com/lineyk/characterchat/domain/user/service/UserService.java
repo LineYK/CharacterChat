72f9dcb5f4bb60e7c4336299500877674dc4c909
LineYK <yhoung11@gmail.com>
2026-03-15 01:58:29 +0900

    feat: UserService signUp 추가

---
+++ src/main/java/com/lineyk/characterchat/domain/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.lineyk.characterchat.domain.user.service;
+
+import com.lineyk.characterchat.domain.user.dto.SignUpRequest;
+import com.lineyk.characterchat.domain.user.dto.UserResponse;
+import com.lineyk.characterchat.domain.user.entity.User;
+import com.lineyk.characterchat.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserResponse signUp(SignUpRequest request) {
+        User savedUser = userRepository.save(request.toEntity(passwordEncoder));
+        return UserResponse.from(savedUser);
+    }
+}
