3c061ce33ac0b1430f70c08b1dd4f1535b4f1cff
LineYK <yhoung11@gmail.com>
2026-03-30 00:19:54 +0900

    feat: SecurityConfig 추가 설정 추가 - formLogin 비활성화 - httpBasic 비활성화 - 세션 비활성화 - 인가 규칙 설정

--- src/main/java/com/lineyk/characterchat/global/config/SecurityConfig.java
+++ src/main/java/com/lineyk/characterchat/global/config/SecurityConfig.java
@@ -1,25 +1,39 @@
 package com.lineyk.characterchat.global.config;
 
+import com.lineyk.characterchat.global.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
-                        auth -> auth.anyRequest().permitAll()
-                );
+                        auth -> auth
+                                .requestMatchers("/api/users/signup", "/api/users/login", "/error").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
