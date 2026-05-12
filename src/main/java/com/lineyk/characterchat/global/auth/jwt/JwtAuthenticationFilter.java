6fc0e6e1970b829efabe7e514e692ddbd20b9e03
LineYK <yhoung11@gmail.com>
2026-05-13 01:27:48 +0900

    refactor: 불필요한 import 문 제거

--- src/main/java/com/lineyk/characterchat/global/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/lineyk/characterchat/global/auth/jwt/JwtAuthenticationFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -16,7 +15,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @Slf4j
 @Component
